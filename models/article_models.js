aea3f391affab66ffa37be1896ff25d7eee720af
wttocs <scottw114@hotmail.co.uk>
2022-07-05 11:31:34 +0100

    7: Added functionality for GET/api/articles/:article_id (comment_count)

--- models/article_models.js
+++ models/article_models.js
@@ -4,11 +4,17 @@ const format = require("pg-format");
 // Trello 4
 exports.fetchArticleById = (params) => {
   const { article_id } = params;
-  const queryString = "SELECT * FROM articles WHERE article_id = $1;";
+  const queryValues = [];
+  let queryString = `SELECT articles.*, COUNT(comments.comment_id)::INT AS comment_count 
+    FROM articles 
+    LEFT JOIN comments USING (article_id) WHERE articles.article_id = $1 GROUP BY articles.article_id`;
+
   return db.query(queryString, [article_id]).then(({ rows: article }) => {
-    if (article[0] === undefined) {
+    if (!article[0]) {
       return Promise.reject({ status: 404, msg: "Article ID Not Found" });
-    } else return article[0];
+    } else {
+      return article[0];
+    }
   });
 };
 // Trello 5
