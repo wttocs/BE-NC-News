d36270ea79bc8f986f606277ffe5b35e89fe1a23
wttocs <scottw114@hotmail.co.uk>
2022-07-06 14:59:00 +0100

    12: Added functionality for Delete api/comments/:commentid with TDD

--- models/comment_models.js
+++ models/comment_models.js
@@ -82,3 +82,22 @@ exports.insertCommentByArticleId = (article_id, username, body) => {
     }
   });
 };
+// Trello 12
+exports.removeCommentByCommentId = (comment_id) => {
+  const queryString = `
+    DELETE FROM comments
+    WHERE comment_id = $1
+    RETURNING *
+  `;
+  return db
+    .query(queryString, [comment_id])
+    .then(({ rows: commentToDelete }) => {
+      if (!commentToDelete[0]) {
+        return Promise.reject({
+          status: 400,
+          msg: "Bad Request: No comment to delete",
+        });
+      }
+      return commentToDelete;
+    });
+};
